c5d71dfc75eaa73b804dda193c23976d011daf53
Moh. Adam Rahmatullah <37463891+mohadam83@users.noreply.github.com>
2020-05-23 22:50:40 +0700

    final commit code
    final

--- adamdonasi.sql
+++ adamdonasi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 23 Bulan Mei 2020 pada 17.07
+-- Waktu pembuatan: 23 Bulan Mei 2020 pada 17.50
 -- Versi server: 10.1.39-MariaDB
 -- Versi PHP: 7.3.5
 
@@ -44,7 +44,32 @@ INSERT INTO `donasi` (`id`, `donatur`, `kategori`, `detail`, `waktu`) VALUES
 (3, 'adam', 'uang tunai', 'Rp. 100.000,00', '2020-05-23 13:19:20'),
 (4, 'adam', 'bahan makanan', 'beras', '2020-05-23 13:19:20'),
 (5, 'adam', 'obat', 'paracetamol', '2020-05-23 13:25:01'),
-(6, 'bin', 'uang tunai', '$ 10.000', '2020-05-23 14:52:54');
+(6, 'bin', 'uang tunai', '$ 10.000', '2020-05-23 14:52:54'),
+(7, 'trea', 'obat', 'amoxan', '2020-05-23 15:23:44'),
+(8, 'trea', 'bahan makanan', 'mie instan', '2020-05-23 15:23:44'),
+(9, 'hamba allah', 'uang tunai', 'Rp. 50.000', '2020-05-23 15:24:18');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `username`, `password`) VALUES
+(1, 'a', 'adammm', 'adam_aremania83@yahoo.co.id', 'a'),
+(3, 'adam', 'adam_aremania83@yahoo.co.id', 'adam87', 'adamadam');
 
 --
 -- Indexes for dumped tables
@@ -57,6 +82,12 @@ ALTER TABLE `donasi`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
@@ -64,7 +95,13 @@ ALTER TABLE `donasi`
 -- AUTO_INCREMENT untuk tabel `donasi`
 --
 ALTER TABLE `donasi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
